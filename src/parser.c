2305158e08d47ed544befeb2a9bed809b2475d9f
Brian Ward <bward@flatironinstitute.org>
2026-01-21 10:52:07 -0500

    Bump version

--- src/parser.c
+++ src/parser.c
@@ -28544,7 +28544,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_stan(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 2,
-      .patch_version = 4,
+      .patch_version = 5,
     },
   };
   return &language;

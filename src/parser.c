2ada9c18cd3a00125f6c1dd605019a607308ea82
Brian Ward <bward@flatironinstitute.org>
2026-01-21 11:17:19 -0500

    abi 14 for anycode

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 642
 #define LARGE_STATE_COUNT 30
 #define SYMBOL_COUNT 239
@@ -4752,7 +4752,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 83},
   [2] = {.lex_state = 6},
@@ -28519,7 +28519,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_stan(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -28539,13 +28538,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_stan(void) {
     .keyword_lex_fn = ts_lex_keywords,
     .keyword_capture_token = sym_identifier,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "stan",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 2,
-      .patch_version = 5,
-    },
   };
   return &language;
 }

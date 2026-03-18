3af277a6d0dcc01097107e781a3e037885a3071b
Brian Ward <bward@flatironinstitute.org>
2026-03-18 13:30:44 -0400

    Update CI

--- crater.sh
+++ crater.sh
@@ -2,7 +2,7 @@
 
 set -xeo pipefail
 
-find ~/Dev/ml/stanc3/test/integration/good/ -type f -name '*.stan' -exec tree-sitter parse {} \; > out 2>&1
+find ~/Dev/ml/stanc3/test/integration/good/ -type f -name '*.stan*' -exec tree-sitter parse {} \; > out 2>&1
 notify-send "done"
 grep "ERR" out
 rm out

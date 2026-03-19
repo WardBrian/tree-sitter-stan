e09f1c20c3c30c94c3327b8d6146eb8d22e2aff0
Brian Ward <bward@flatironinstitute.org>
2026-03-19 09:34:01 -0400

    Clean up

--- crater.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xeo pipefail
-
-find ~/Dev/ml/stanc3/test/integration/good/ -type f -name '*.stan*' -exec tree-sitter parse {} \; > out 2>&1
-notify-send "done"
-grep "ERR" out
-rm out
